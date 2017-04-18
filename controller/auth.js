110b137049f95de1428eb84e6edf8b676822f49e
Eguono <efeguono@gmail.com>
2017-04-18 16:57:58 +0100

    Initializer Firebase App

---
+++ controller/auth.js
@@ -0,0 +1,4 @@
+const firebase = require("../helper/firebase.js");
+const db = firebase.database();
+const auth = firebase.auth();
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv').config();
 
 const webRoutes = require("./routes/routes.js");
 
