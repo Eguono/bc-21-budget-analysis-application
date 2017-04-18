07a07491d41f09841e041f7f5998b1024a629399
Eguono <efeguono@gmail.com>
2017-04-18 16:16:46 +0100

    Setup bodyParser Middleware and public static directory

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express"),
     bodyParser = require("body-parser");
 
+//Initialize Express App
 const app = express();
+const route = express.Router();
 
+
+//listening on port 5000
 const port = 5000 || process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Working!");
-});
+//setup static directory
+express.static("public");
 
-app.listen(port, ()=>{
+//Setup ejs as view engine
+app.set('views', process.cwd() + '/views');
+app.set('view engine', 'ejs');
+
+//using bodyParser as middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//routes
+route("/")
+    .get((req, res) => {
+        res.send("Working!");
+    });
+
+app.listen(port, () => {
     console.log("Listening on port: " + port);
 });
