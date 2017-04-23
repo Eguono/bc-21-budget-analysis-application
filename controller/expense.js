ad45b376ead331b7052c6ee946841b204b986fec
Eguono <efeguono@gmail.com>
2017-04-23 05:41:27 +0100

    add required to input field

--- controller/expense.js
+++ controller/expense.js
@@ -8,44 +8,55 @@ module.exports.addExpense = (req, res) => {
     let description = req.body.expensedescription;
     let expenseType = req.body.expensetype;
 
-    let user = auth.currentUser;
-    if (user) {
-        let userId = user.uid;
-        ref.child("totalExpense/" + userId + "/totalexpense").once("value", (snap) => {
-            let result = {};
-            let expensesRef = ref.child('totalExpense/' + userId);
-            let expenseRef = expensesRef.push();
-            let expenseKey = expenseRef.key;
-            let today = new Date();
-            let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-            let data = {
-                amount: amount,
-                description: description,
-                expenseType: expenseType,
-                date: date,
-                id: expenseKey
-            };
-            if (snap.val() === null) {
-                result["totalExpense/" + userId + "/totalexpense"] = amount;
-                result["expense/" + userId + "/" + expenseKey] = data;
+    req.checkBody('expenseamount', 'Invalid Expense Amount').notEmpty().isInt();
 
-                ref.update(result);
-                return res.redirect('/dashboard');
-            } else {
-                let totalExpense = Number(snap.val()) + Number(amount);
-                result["totalExpense/" + userId + "/totalexpense"] = totalExpense;
-                result["expense/" + userId + "/" + expenseKey] = data;
+    let errors = req.validationErrors();
 
-                ref.update(result);
+    if (errors) {
+        console.log(errors)
+        let errormsg = errors[0].msg;
+        return res.redirect('dashboard');
+    } else {
+
+        let user = auth.currentUser;
+        if (user) {
+            let userId = user.uid;
+            ref.child("totalExpense/" + userId + "/totalexpense").once("value", (snap) => {
+                let result = {};
+                let expensesRef = ref.child('totalExpense/' + userId);
+                let expenseRef = expensesRef.push();
+                let expenseKey = expenseRef.key;
+                let today = new Date();
+                let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                let data = {
+                    amount: amount,
+                    description: description,
+                    expenseType: expenseType,
+                    date: date,
+                    id: expenseKey
+                };
+                if (snap.val() === null) {
+                    result["totalExpense/" + userId + "/totalexpense"] = amount;
+                    result["expense/" + userId + "/" + expenseKey] = data;
+
+                    ref.update(result);
+                    return res.redirect('/dashboard');
+                } else {
+                    let totalExpense = Number(snap.val()) + Number(amount);
+                    result["totalExpense/" + userId + "/totalexpense"] = totalExpense;
+                    result["expense/" + userId + "/" + expenseKey] = data;
+
+                    ref.update(result);
+                    res.redirect('/dashboard');
+                }
+
+            }, (err) => {
+                var errorCode = err.code;
+                var errorMessage = err.message;
+                console.log(err);
                 res.redirect('/dashboard');
-            }
-
-        }, (err) => {
-            var errorCode = err.code;
-            var errorMessage = err.message;
-            console.log(err);
-            res.redirect('/dashboard');
-        });
+            });
+        }
     }
 
 }
@@ -77,9 +88,9 @@ module.exports.deleteExpense = (req, res) => {
 
     let incomeRefs = ref.child("expense/" + userId + "/" + id)
     incomeRefs.set(null);
-    ref.child("totalExpense/" + userId + "/totalexpense").once("value", (snap) => { 
-        let newTotal = snap.val()-amount;
-        ref.child("totalExpense/" + userId ).update({totalexpense: newTotal});
+    ref.child("totalExpense/" + userId + "/totalexpense").once("value", (snap) => {
+        let newTotal = snap.val() - amount;
+        ref.child("totalExpense/" + userId).update({ totalexpense: newTotal });
         res.redirect('/expense');
     }, (err) => {
         var errorCode = err.code;
--- controller/income.js
+++ controller/income.js
@@ -8,58 +8,93 @@ module.exports.addIncome = (req, res) => {
     let description = req.body.description;
     let incomeType = req.body.incometype;
 
+    req.checkBody('amount', 'Invalid Income Amount').notEmpty().isInt();
 
-    let user = auth.currentUser;
-    let userId = user.uid;
+    let errors = req.validationErrors();
 
-    ref.child("totalIncome/" + userId + "/totalincome").once("value", (snap) => {
-        let result = {};
-        let incomesRef = ref.child('totalIncome/' + userId);
-        let incomeRef = incomesRef.push();
-        let incomeKey = incomeRef.key;
-        let today = new Date();
-        let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-        let data = {
-            amount: amount,
-            description: description,
-            incomeType: incomeType,
-            date: date,
-            id: incomeKey
-        };
+    if (errors) {
+        console.log(errors)
+        let errormsg = errors[0].msg;
+        return res.redirect('dashboard');
+    } else {
 
-        if (snap.val() === null) {
-            result["totalIncome/" + userId + "/totalincome"] = amount;
-            result["income/" + userId + "/" + incomeKey] = data;
 
-            ref.update(result);
-            return res.redirect('/dashboard');
-        } else {
-            let totalIncome = Number(snap.val()) + Number(amount);
+        let user = auth.currentUser;
+        let userId = user.uid;
 
-            result["totalIncome/" + userId + "/totalincome"] = totalIncome;
-            result["income/" + userId + "/" + incomeKey] = data;
+        ref.child("totalIncome/" + userId + "/totalincome").once("value", (snap) => {
+            let result = {};
+            let incomesRef = ref.child('totalIncome/' + userId);
+            let incomeRef = incomesRef.push();
+            let incomeKey = incomeRef.key;
+            let today = new Date();
+            let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+            let data = {
+                amount: amount,
+                description: description,
+                incomeType: incomeType,
+                date: date,
+                id: incomeKey
+            };
 
-            ref.update(result);
+            if (snap.val() === null) {
+                result["totalIncome/" + userId + "/totalincome"] = amount;
+                result["income/" + userId + "/" + incomeKey] = data;
+
+                ref.update(result);
+                return res.redirect('/dashboard');
+            } else {
+                let totalIncome = Number(snap.val()) + Number(amount);
+
+                result["totalIncome/" + userId + "/totalincome"] = totalIncome;
+                result["income/" + userId + "/" + incomeKey] = data;
+
+                ref.update(result);
+                res.redirect('/dashboard');
+            }
+
+        }, (err) => {
+            var errorCode = err.code;
+            var errorMessage = err.message;
+            console.log(err);
             res.redirect('/dashboard');
-        }
+        });
+    }
 
-    }, (err) => {
-        var errorCode = err.code;
-        var errorMessage = err.message;
-        console.log(err);
-        res.redirect('/dashboard');
-    });
 }
 
 
+    module.exports.displayIncome = (req, res) => {
+        let user = auth.currentUser;
+        if (user) {
+            let userId = user.uid;
+            ref.child("income/" + userId).once("value", (snap) => {
+                res.render("income", { data: snap.val() })
+            }, (err) => {
+                var errorCode = err.code;
+                var errorMessage = err.message;
+                console.log(err);
+                res.redirect('/dashboard');
+            });
 
+        } else {
+            res.render("login", { error: "Please Login To Use the Web App" });
+        }
 
-module.exports.displayIncome = (req, res) => {
-    let user = auth.currentUser;
-    if (user) {
+    }
+
+    module.exports.deleteIncome = (req, res) => {
+        let user = auth.currentUser;
         let userId = user.uid;
-        ref.child("income/" + userId).once("value", (snap) => {
-            res.render("income", { data: snap.val() })
+        let id = req.query.id;
+        let amount = req.query.amount;
+
+        let incomeRefs = ref.child("income/" + userId + "/" + id)
+        incomeRefs.set(null);
+        ref.child("totalIncome/" + userId + "/totalincome").once("value", (snap) => {
+            let newTotal = snap.val() - amount;
+            ref.child("totalIncome/" + userId).update({ totalincome: newTotal });
+            res.redirect('/income');
         }, (err) => {
             var errorCode = err.code;
             var errorMessage = err.message;
@@ -67,29 +102,4 @@ module.exports.displayIncome = (req, res) => {
             res.redirect('/dashboard');
         });
 
-    } else {
-        res.render("login", { error: "Please Login To Use the Web App" });
     }
-
-}
-
-module.exports.deleteIncome = (req, res) => {
-    let user = auth.currentUser;
-    let userId = user.uid;
-    let id = req.query.id;
-    let amount = req.query.amount;
-
-    let incomeRefs = ref.child("income/" + userId + "/" + id)
-    incomeRefs.set(null);
-    ref.child("totalIncome/" + userId + "/totalincome").once("value", (snap) => {
-        let newTotal = snap.val() - amount;
-        ref.child("totalIncome/" + userId).update({ totalincome: newTotal });
-        res.redirect('/income');
-    }, (err) => {
-        var errorCode = err.code;
-        var errorMessage = err.message;
-        console.log(err);
-        res.redirect('/dashboard');
-    });
-
-}
